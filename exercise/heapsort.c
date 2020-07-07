d2b592fb5ddd3cabd6cd18ff11bbdb8f02f31664
vviseman <vviseman@MacBook-Pro.local>
2020-07-08 00:23:22 +0900

    edit file

--- exercise/heapsort.c
+++ exercise/heapsort.c
@@ -2,6 +2,12 @@
 
 //下に行くほど値が大きくなるor小さくなる
 
+/*
+1.要素数Nのヒープを、「子よりも親が大きい」として構築：根が最大値となる
+2.根を配列の最後尾と入れ替える。
+3.N - 1個の要素のヒープを構築しなおす：根が2番目に大きい値となる
+*/
+
 /* 値を入れ替える関数 */
 void swap(int *x, int *y) {
     int temp;  // 値を一時保存する変数
@@ -17,9 +23,9 @@ void pushdown(int array[], int first, int last) {
     int child = 2 * parent;  //左の子
     while (child <= last) {
         if ((child < last) && (array[child] < array[child + 1])) {  //array[child] : 左側, array[child + 1] : 右側
-            child++; // 右の子の方が大きいとき、右の子を比較対象に設定
+            child++;  // 右の子の方が大きいとき、右の子を比較対象に設定 if文の(child + 1)を(child++)で表現している
         }
-        if (array[child] <= array[parent]) {//ヒープソートの条件と一致
+        if (array[child] <= array[parent]) {//ヒープソートの条件と一致 親が子より大きければヒープ済み
             break; // ヒープ済み
         }
         swap(&array[child], &array[parent]);//親と子の入れ替え
@@ -32,11 +38,11 @@ void pushdown(int array[], int first, int last) {
 void heap_sort(int array[], int array_size) {
     int i;
 
-    for (i = array_size / 2; i >= 1; i--) {
+    for (i = array_size / 2; i >= 1; i--) {//最下層の要素になるまで
         pushdown(array, i, array_size);  // 全体をヒープ化
     }
     for (i = array_size; i >= 2; i--) {
-        swap(&array[1], &array[i]);  // 最大のものを最後に
+        swap(&array[1], &array[i]);  // 最大のものを最後に 根(配列の先頭、N[0])に必ず最大値が来るため
         pushdown(array, 1, i - 1);   // ヒープ再構築
     }
 }
