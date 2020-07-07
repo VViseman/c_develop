0b863c5401fa87cbf5eb56d634504f5a24f6cc77
vviseman <vviseman@MacBook-Pro.local>
2020-07-07 23:01:07 +0900

    edit files

--- exercise/heapsort.c
+++ exercise/heapsort.c
@@ -1,62 +1,63 @@
 #include <stdio.h>
 
-#define N 10//グローバル変数
-
+//下に行くほど値が大きくなる
 
 /* 値を入れ替える関数 */
-void swap (int *x, int *y) {
-  int temp;    // 値を一時保存する変数
+void swap(int *x, int *y) {
+    int temp;  // 値を一時保存する変数
 
-  temp = *x;
-  *x = *y;
-  *y = temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
 }
 
-/* pushdouwn操作 */
-void pushdown (int array[], int first, int last) {
-  int parent = first;   // 親
-  int child = 2*parent;   // 左の子
-  while (child <= last) {
-    if ((child < last) && (array[child] < array[child+1])) {
-       child++;   // 右の子の方が大きいとき、右の子を比較対象に設定
+/*下に下げながらヒープを構築する位置までおろしていくpushdouwn操作*/
+void pushdown(int array[], int first, int last) {
+    int parent = first;      // 親
+    int child = 2 * parent;  // 左の子
+    while (child <= last) {
+        if ((child < last) && (array[child] < array[child + 1])) {
+            child++;  // 右の子の方が大きいとき、右の子を比較対象に設定
+        }
+        if (array[child] <= array[parent]) {
+            break;
+        }  // ヒープ済み
+        swap(&array[child], &array[parent]);
+        parent = child;//深くしていく
+        child = 2 * parent;//深くしていく
     }
-    if (array[child] <= array[parent]) { break; }   // ヒープ済み
-    swap(&array[child], &array[parent]);
-    parent = child;
-    child = 2* parent;
-  }
 }
 
 /* ヒープソート */
-void heap_sort (int array[], int array_size) {
-  int i;
+void heap_sort(int array[], int array_size) {
+    int i;
 
-  for (i = (array_size - 1)/2; i >= 0; i--) {
-    pushdown(array, i, array_size - 1);   // 全体をヒープ化
-  }
-  for (i = (array_size - 1); i >= 1; i--) {
-    swap(&array[1], &array[i]);   // 最大のものを最後に
-    pushdown(array, 1, i-1);   // ヒープ再構築
-  }
+    for (i = array_size / 2; i >= 1; i--) {
+        pushdown(array, i, array_size);  // 全体をヒープ化
+    }
+    for (i = array_size; i >= 2; i--) {
+        swap(&array[1], &array[i]);  // 最大のものを最後に
+        pushdown(array, 1, i - 1);   // ヒープ再構築
+    }
 }
 
-int main (void) {
-  int array[N] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-  int i;
+int main(void) {
+    int array[11] = {0, 2, 1, 8, 5, 4, 7, 9, 10, 6, 3};
+    int i;
 
-  printf("Before sort: ");
-  for (i = 0; i < N; i++) {
-      printf("%d ", array[i]);
-  }
-  printf("\n");
+    printf("Before sort: ");
+    for (i = 1; i < 11; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
 
-  heap_sort(array, N);
+    heap_sort(array, 10);
 
-  printf("After  sort: ");
-  for (i = 0; i < N; i++) {
-      printf("%d ", array[i]);
-  }
-  printf("\n");
+    printf("After sort: ");
+    for (i = 1; i < 11; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
 
-  return 0;
+    return 0;
 }
